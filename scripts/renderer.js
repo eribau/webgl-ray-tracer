3b50b198975dc98dd10cb186749b0fca19ea41c2
eribau <eribau@users.noreply.github.com>
2022-07-07 18:54:31 +0000

    deploy: cdafc9cfe62fa795628530555050525cbdd60747

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -54,6 +54,19 @@ const degreesToRadians = glsl`
       float s = 1e-8;
       return (vec.x < s) && (vec.y < s) && (vec.z < s);
    }
+
+   vec3 refraction(vec3 uv, vec3 n, float etai_over_etat) {
+      float cos_theta = min(dot(-uv, n), 1.0);
+      vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
+      vec3 r_out_parallel = -sqrt(abs(1.0 - dot(r_out_perp, r_out_perp)))*n;
+      return r_out_perp + r_out_parallel;
+   }
+
+   float reflectance(float cosine, float ref_idx) {
+      float r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+      r0 = r0*r0;
+      return r0 + (1.0 - r0)*pow((1.0 - cosine), 5.0);
+   }
 `;
 
 const random = glsl`
@@ -66,6 +79,11 @@ const random = glsl`
       uint h32 = 1103515245U*((p.x)^(p.y>>3U));
       return h32^(h32 >> 16);
    }
+
+   float hash1(inout float seed) {
+      uint n = base_hash(floatBitsToUint(vec2(seed+=.1,seed+=.1)));
+      return float(n)*(1.0/float(0xffffffffU));
+  }
 
    vec2 hash2(inout float seed) {
        uint n = base_hash(floatBitsToUint(vec2(seed+=.1,seed+=.1)));
@@ -124,9 +142,10 @@ const camera = glsl`
 
 const material = glsl`
    struct Material {
-      int material; // 0 = lambertian, 1 = metal
+      int material; // 0 = lambertian, 1 = metal, 2 = dielectric
       vec3 albedo;
-      float fuzz;
+      float fuzz; 
+      float ir; // index of reflection for dielectrics
    };
 `;
 
@@ -164,6 +183,27 @@ const scatter = glsl`
       scattered = Ray(rec.p, reflected + rec.material.fuzz*random_in_unit_sphere(g_seed));
       attenuation = rec.material.albedo;
       return (dot(scattered.direction, rec.normal) > 0.0);
+   }
+
+   bool dielectric_scatter(in Ray r_in, in Hit_record rec, inout vec3 attenuation, inout Ray scattered) {
+      attenuation = vec3(1.0);
+      float refraction_ratio = rec.front_face ? (1.0/rec.material.ir) : rec.material.ir;
+
+      vec3 unit_direction = normalize(r_in.direction);
+      float cos_theta = min(dot(-unit_direction, rec.normal), 1.0);
+      float sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+      bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+      vec3 direction;
+
+      if(cannot_refract || reflectance(cos_theta, refraction_ratio) > hash1(g_seed)) {
+         direction = reflect(unit_direction, rec.normal);
+      } else {
+         direction = refract(unit_direction, rec.normal, refraction_ratio);
+      }
+
+      scattered = Ray(rec.p, direction);
+      return true;
    }
 `;
 
@@ -249,16 +289,21 @@ const rayColor = glsl`
                case 0:
                   if(lambertian_scatter(rec, attenuation, scattered)) {
                      color *= attenuation;
-
                      r = scattered;
                   }
                   break;
                case 1:
                   if(metal_scatter(r, rec, attenuation, scattered)) {
                      color *= attenuation;
-
                      r = scattered;
                   }
+                  break;
+               case 2:
+                  if(dielectric_scatter(r, rec, attenuation, scattered)) {
+                     color *= attenuation;
+                     r = scattered;
+                  }
+                  break;
             }
          } else {
             vec3 unit_direction = normalize(r.direction);
@@ -277,10 +322,10 @@ const fragmentShaderMain = glsl`
       float aspect = resolution.x / resolution.y;
 
       // Materials
-      Material material_ground = Material(0, vec3(0.8, 0.8, 0.0), 0.0);
-      Material material_center = Material(0, vec3(0.7, 0.3, 0.3), 0.0);
-      Material material_left = Material(1, vec3(0.8), 0.3);
-      Material material_right = Material(1, vec3(0.8, 0.6, 0.2), 1.0);
+      Material material_ground = Material(0, vec3(0.8, 0.8, 0.0), 0.0, 0.0);
+      Material material_center = Material(0, vec3(0.1, 0.2, 0.5), 0.0, 0.0);
+      Material material_left = Material(2, vec3(0.8, 0.8, 0.8), 0.3, 1.5);
+      Material material_right = Material(1, vec3(0.8, 0.6, 0.2), 0.0, 0.0);
 
       // World
       Sphere spheres[4];
@@ -379,183 +424,6 @@ var renderFragmentSource = glsl`#version 300 es
    void main() {
       fragColor = texture(u_texture, v_texcoord);
    }
-`;
-
-// Shader taken from https://www.shadertoy.com/view/llVcDz
-var reinFragmentSource = glsl`#version 300 es
-   precision mediump float;
-   #define MAX_FLOAT 1e5
-   #define MAX_RECURSION 5
-
-   uniform float time;
-   uniform float textureWeight;
-   uniform sampler2D u_texture;
-   out vec4 frag_color;
-
-   // Image
-   const float aspect_ratio = 16.0 / 9.0;
-   const float image_width = 600.0;
-   const float image_height = image_width / aspect_ratio;
-   const float viewport_height = 2.0;
-   const float viewport_width = viewport_height * aspect_ratio;
-   const float focal_length = 1.0;
-   const int samples_per_pixel = 1;
-   const int max_depth = 50;
-
-   //
-   // Hash functions by Nimitz:
-   // https://www.shadertoy.com/view/Xt3cDn
-   //
-
-   uint base_hash(uvec2 p) {
-      p = 1103515245U*((p >> 1U)^(p.yx));
-      uint h32 = 1103515245U*((p.x)^(p.y>>3U));
-      return h32^(h32 >> 16);
-   }
-
-   float g_seed = 0.;
-
-   vec2 hash2(inout float seed) {
-      uint n = base_hash(floatBitsToUint(vec2(seed+=.1,seed+=.1)));
-      uvec2 rz = uvec2(n, n*48271U);
-      return vec2(rz.xy & uvec2(0x7fffffffU))/float(0x7fffffff);
-   }
-
-   vec3 hash3(inout float seed) {
-      uint n = base_hash(floatBitsToUint(vec2(seed+=.1,seed+=.1)));
-      uvec3 rz = uvec3(n, n*16807U, n*48271U);
-      return vec3(rz & uvec3(0x7fffffffU))/float(0x7fffffff);
-   }
-
-   //
-   // Ray trace helper functions
-   //
-
-   float schlick(float cosine, float ior) {
-      float r0 = (1.-ior)/(1.+ior);
-      r0 = r0*r0;
-      return r0 + (1.-r0)*pow((1.-cosine),5.);
-   }
-
-   vec3 random_in_unit_sphere(inout float seed) {
-      vec3 h = hash3(seed) * vec3(2.,6.28318530718,1.)-vec3(1,0,0);
-      float phi = h.y;
-      float r = pow(h.z, 1./3.);
-      return r * vec3(sqrt(1.-h.x*h.x)*vec2(sin(phi),cos(phi)),h.x);
-   }
-
-   //
-   // Ray
-   //
-
-   struct ray {
-      vec3 origin, direction;
-   };
-      
-   //
-   // Hit record
-   //
-
-   struct hit_record {
-      float t;
-      vec3 p, normal;
-   };
-
-   //
-   // Hitable, for now this is always a sphere
-   //
-
-   struct hitable {
-      vec3 center;
-      float radius;
-   };
-
-   bool hitable_hit(const in hitable hb, const in ray r, const in float t_min, 
-                  const in float t_max, inout hit_record rec) {
-      // always a sphere
-      vec3 oc = r.origin - hb.center;
-      float b = dot(oc, r.direction);
-      float c = dot(oc, oc) - hb.radius * hb.radius;
-      float discriminant = b * b - c;
-      if (discriminant < 0.0) return false;
-
-      float s = sqrt(discriminant);
-      float t1 = -b - s;
-      float t2 = -b + s;
-      
-      float t = t1 < t_min ? t2 : t1;
-      if (t < t_max && t > t_min) {
-         rec.t = t;
-         rec.p = r.origin + t*r.direction;
-         rec.normal = (rec.p - hb.center) / hb.radius;
-         return true;
-      } else {
-         return false;
-      }
-   }
-
-   //
-   // Camera
-   //
-
-   struct camera {
-      vec3 origin, lower_left_corner, horizontal, vertical;
-   };
-
-   ray camera_get_ray(camera c, vec2 uv) {
-      return ray(c.origin, 
-                  normalize(c.lower_left_corner + uv.x*c.horizontal + uv.y*c.vertical - c.origin));
-   }
-
-   //
-   // Color & Scene
-   //
-
-   bool world_hit(const in ray r, const in float t_min, const in float t_max, out hit_record rec) {
-      rec.t = t_max;
-      bool hit = false;
-      
-      hit = hitable_hit(hitable(vec3(0,0,-1), .5), r, t_min, rec.t, rec) || hit;
-      hit = hitable_hit(hitable(vec3(0,-100.5,-1),100.), r, t_min, rec.t, rec) || hit;
-      
-      return hit;
-   }
-
-   vec3 color(in ray r) {
-      vec3 col = vec3(1);  
-      hit_record rec;
-      
-      for (int i=0; i<MAX_RECURSION; i++) {
-         if (world_hit(r, 0.001, MAX_FLOAT, rec)) {
-            vec3 rd = normalize(rec.normal + random_in_unit_sphere(g_seed));
-               col *= .5;
-
-               r.origin = rec.p;
-               r.direction = rd;
-         } else {
-               float t = .5*r.direction.y + .5;
-               col *= mix(vec3(1),vec3(.5,.7,1), t);
-               return col;
-         }
-      }
-      return col;
-   }
-
-   //
-   // Main
-   //
-
-   void main() {
-      vec2 resolution = vec2(600, 338);
-      
-      g_seed = float(base_hash(floatBitsToUint(gl_FragCoord.xy)))/float(0xffffffffU)+time;
-      vec2 uv = (gl_FragCoord.xy + hash2(g_seed))/resolution.xy;
-      float aspect = resolution.x/resolution.y;
-      ray r = camera_get_ray(camera(vec3(0), vec3(-2,-1,-1), vec3(4,0,0), vec3(0,4./aspect,0)), uv);
-      vec3 col = color(r);
-      vec3 texture = texture(u_texture, gl_FragCoord.xy / resolution).rgb;
-      frag_color = vec4(mix(sqrt(col), texture, textureWeight), 1.0);
-   }   
 `;
 
 // Object classes
